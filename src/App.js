5cd28227f70903f566516814f5d1b17cc1fadb95
Łukasz Badocha <krakow@akademia108.pl>
2019-06-13 11:36:35 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Countdown from 'react-countdown-now';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+
+	constructor() {
+		super();
+
+		this.renderer = ({ days, hours, minutes, seconds}) => {
+			return <span>{days} dni {hours} godzin {minutes} minut {seconds} sekund</span>;
+		}
+
+	}
+  
+  render() {
+
+		let newDate = new Date();
+		console.log(newDate);
+    return (
+      <div className="App">
+        <header className="App-header">
+					
+					<h1>Zostało</h1>
+					<h2><Countdown date={'Mon, 12 Aug 2019 18:00:00'} renderer={this.renderer} /></h2>
+        </header>
+      </div>
+    );
+  }
+  
+  
 }
 
 export default App;
